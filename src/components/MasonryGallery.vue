7319c3d458f2caccf9b8f1059ce38f5de193a662
johanpoyet <jojopoy2003@gmail.com>
2025-04-30 09:59:00 +0200

    push

---
+++ src/components/MasonryGallery.vue
@@ -0,0 +1,13 @@
+<template>
+  <MasonryWall :items="items" :column-width="300" :gap="10">
+    <template #default="{ item }">
+      <GalleryItem :item="item" />
+    </template>
+  </MasonryWall>
+</template>
+
+<script setup>
+import MasonryWall from 'vue-masonry-wall'
+import GalleryItem from './GalleryItem.vue'
+defineProps(['items'])
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-wrap justify-center gap-4 py-4">
+    <button
+      v-for="cat in categories"
+      :key="cat"
+      @click="$emit('update:selected', cat)"
+      :class="['uppercase font-bold', selected === cat ? 'text-white' : 'text-gray-400']"
+    >
+      {{ cat }}
+    </button>
+  </div>
+</template>
+
+<script setup>
+defineProps(['categories', 'selected'])
+defineEmits(['update:selected'])
+</script>
