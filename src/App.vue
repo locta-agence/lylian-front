f0c4329b40aaf1f50f42f20a27b097b46716030a
Jordan <olivier.jordan21@gmail.com>
2025-04-30 15:07:34 +0200

    homepage & menu component

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,12 @@
 
 <script setup lang="ts">
-import { onMounted } from 'vue'
-import { useGalleryStore } from '@/stores/gallery'
+import { RouterLink, RouterView } from 'vue-router'
 
-const galleryStore = useGalleryStore()
-
-onMounted(() => {
-  galleryStore.loadGalleries()
-})
-
-console.log(galleryStore.galleries)
 </script>
 
 <template>
-  <!-- <div>
-    <h2>Catégories</h2>
-    <ul>
-      <li
-        v-for="category in galleryStore.categories"
-        :key="category"
-        @click="galleryStore.setCategory(category)"
-      >
-        {{ category }}
-      </li>
-    </ul>
-
-    <h2>Images de la catégorie "{{ galleryStore.currentCategory }}"</h2>
-    <div v-if="galleryStore.images.length">
-      <img
-        v-for="img in galleryStore.images"
-        :key="img"
-        :src="img"
-        style="width: 200px; margin: 5px"
-      />
-    </div>
-
-    <h2>Toutes les images</h2>
-    <div>
-      <img
-        v-for="img in galleryStore.allImages"
-        :key="img"
-        :src="img"
-        style="width: 100px; margin: 3px"
-      />
-    </div>
-  </div> -->
-  <div>
-    
-    <router-view />
-    
-  </div>
-  
+  <RouterView />
 </template>
+
+<style scoped>
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,49 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <main>
-    <h1>Home</h1>
-  </main>
+  <div
+    class="relative h-screen w-screen bg-cover bg-center transition-all duration-1000"
+    :style="{ backgroundImage: `url(${currentBg})` }"
+  >
+    <div class="absolute inset-0 flex flex-col items-center justify-center text-white text-center px-4">
+      <img src="/logo.png" alt="Llianshoots" class="w-1/2 md:w-1/4 mb-2">
+      <h2 class="text-[40px] font-extrabold leading-[100%] text-[#FCFCFC] text-center uppercase">
+        Photographer
+      </h2>
+      <h2 class="text-[40px] font-extrabold leading-[100%] text-[#FCFCFC] text-center uppercase">
+        & Filmmaker
+      </h2>
+    </div>
+
+    <MenuOverlay />
+
+  </div>
 </template>
+
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted } from 'vue'
+import MenuOverlay from '@/components/Layout/MenuOverlay.vue'
+
+const bgImages = [
+  '/bg/bg1.png',
+  '/bg/bg2.png',
+  '/bg/bg3.png',
+  '/bg/bg4.png',
+  '/bg/bg5.png'
+]
+
+const currentIndex = ref(0)
+const currentBg = ref(bgImages[currentIndex.value])
+const showMenu = ref(false)
+
+let interval: NodeJS.Timeout
+
+onMounted(() => {
+  interval = setInterval(() => {
+    currentIndex.value = (currentIndex.value + 1) % bgImages.length
+    currentBg.value = bgImages[currentIndex.value]
+  }, 3000) // 3 secondes
+})
+
+onUnmounted(() => {
+  clearInterval(interval)
+})
+</script>
